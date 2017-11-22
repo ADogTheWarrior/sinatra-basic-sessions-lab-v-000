a4cbd4c5057069215286b93e90c0d4ad64fa3a84
Adam Fudge <coolfudge@gmail.com>
2017-11-22 21:49:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # configure do
-  #   enable :sessions
-  #   set :session_secret, "secret"
-  # end
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+  end
+
+  post '/checkout' do
+  end
 end
